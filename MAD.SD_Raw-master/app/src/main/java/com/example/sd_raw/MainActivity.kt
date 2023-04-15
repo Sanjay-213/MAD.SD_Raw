7ff16825c3c883ad3b85cae3df29a3dad5342714
checkinandout <200701213@rajalakshmi.edu.in>
2023-04-15 18:41:44 +0530

    finish

---
+++ MAD.SD_Raw-master/app/src/main/java/com/example/sd_raw/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.sd_raw
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        val btn: Button = findViewById(R.id.button)
+        btn.setOnClickListener {
+            val intent = Intent(this,FrontPage::class.java)
+            startActivity(intent)
+        }
+    }
+}
